97178c1b1b344897f1ccd77321a316793885ebec
lihao <maigeyouzichi@gmail.com>
2023-01-12 18:38:30 +0800

    spring-mvc传参方式,array和json

--- spring-basic/spring-mvc/src/main/java/com/spring/controller/UserController.java
+++ spring-basic/spring-mvc/src/main/java/com/spring/controller/UserController.java
@@ -2,7 +2,11 @@ package com.spring.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.spring.entity.User;
+import java.util.Arrays;
+import java.util.List;
+import lombok.val;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -73,5 +77,46 @@ public class UserController {
     @ResponseBody
     public String find4(@RequestParam(value = "id",required = false,defaultValue = "2") Long id, @RequestParam("name") String name){
         return "id: "+ id + "name: " + name;
+    }
+
+    /**
+     * 数组传参
+     * <a href="http://localhost:9090/user/list?name=lisi&name=zhangsan&name=wangwu">测试URL</a>
+     */
+    @RequestMapping("/user/list")
+    @ResponseBody
+    public String list(String[] name){
+        val arrString = Arrays.toString(name);
+        System.out.println("arrString: "+ arrString);
+        return arrString;
+    }
+
+
+    /**
+     * 用List 接收，就必须加   @RequestParam("name")，
+     * 从原理上来讲，传参的过程中 第一步就是要实例化对象，很明显 List是不能new的，所以需要springmvc自带的注解来帮忙做这个解析的过程
+     * <a href="http://localhost:9090/user/list2?name=lisi&name=zhangsan&name=wangwu">测试URL</a>
+     */
+    @RequestMapping("/user/list2")
+    @ResponseBody
+    public String list2(@RequestParam("name") List<String> name){
+        System.out.println(name.toString());
+        return name.toString();
+    }
+
+    //todo 复杂对象嵌套传参,省略,不做测试
+    //todo 路径传参,省略,实际工作禁止使用
+
+    /**
+     * json传参条件: 1,post请求 2,content-type为application/json 3,@RequestBody注解
+     * <a href="http://localhost:9090/user/jsonAddress">url</a>
+     * 请求头: content-type=application/json
+     * 请求体:{"id": 1, "name": "zhangsan"}
+     */
+    @RequestMapping("/user/jsonAddress")
+    @ResponseBody
+    public String address(@RequestBody User user){
+        System.out.println(user);
+        return JSON.toJSONString(user);
     }
 }
